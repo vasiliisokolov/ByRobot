b5167c59fbf0bc2465e3c3141c5bb19c7b9eb203
Vasily Sokolov <vasilysokolov@mail.ru>
2022-09-01 18:31:44 +0300

    Добавьте файлы проекта.

---
+++ ByRobot/ByRobot.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int n;
+    int buyers;
+    std::cout << "Enter amount of robots:\n";
+    std::cin >> n;
+    std::vector<int> robots(n);
+    std::cout << "Complete the list of robots:\n";
+    for (int i = 0; i < robots.size(); i++)
+    {
+        std::cin >> robots[i];
+    }
+    std::cout << "Enter amount of buyers:\n";
+    std::cin >> buyers;
+    std::vector<int> purchases(buyers);
+    std::cout << "Complete the list of buyers:\n";
+    for (int i = 0; i < purchases.size(); i++)
+    {
+        std::cin >> purchases[i];
+    }
+    
+
+}
