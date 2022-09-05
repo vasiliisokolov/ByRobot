af19a8476257c0a674a9baf87a05dbdbbddd44d3
VasiliiSokolov <vasilysokolov@mail.ru>
2022-09-05 20:26:09 +0300

    done!

--- ByRobot/ByRobot.cpp
+++ ByRobot/ByRobot.cpp
@@ -2,9 +2,8 @@
 #include <iostream>
 #include <vector>
 
-int main();
-
-std::vector<int> bye_robot(std::vector<int>, int, int);
+std::vector<int> bye_robot(std::vector<int>, int);
+bool check_input(int, int);
 
 int main()
 {
@@ -14,43 +13,62 @@ int main()
     std::cin >> n;
     std::vector<int> robots(n);
     std::cout << "Complete the list of robots:\n";
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < robots.size(); i++)
     {
         std::cin >> robots[i];
     }
     std::cout << "Enter amount of buyers:\n";
     std::cin >> buyers;
-    
-    while (buyers != 0)
+    if (check_input(n, buyers))
     {
-        std::cout << "Enter robot's index:\n";
-        std::cin >> robotIndex;
-        robots = bye_robot(robots, robotIndex, n);
-        std::cout << "Left: ";
-        for (int i = 0; i < robots.size(); i++)
+        while (buyers != 0)
         {
-            std::cout << robots[i] << " ";
+            std::cout << "Enter robot's index:\n";
+            std::cin >> robotIndex;
+            robots = bye_robot(robots, robotIndex);
+            std::cout << "Left: ";
+            for (int i = 0; i < robots.size() - 1; i++)
+            {
+                std::cout << robots[i] << " ";
 
+            }
+            buyers--;
         }
-        buyers--;
+    }
+    else
+    {
+        std::cout << "Error!" << std::endl;
     }
     
 
 }
 
-std::vector<int> bye_robot(std::vector<int> robots, int robotIndex, int n)
+std::vector<int> bye_robot(std::vector<int> robots, int robotIndex)
 {
-    std::vector<int> vec2 (n);
-    int temp = robots[n];
+    std::vector<int> vec2 (robots.size() - 1);
+    
     int i = 0;
     for(; i < robotIndex; i++)
     {
         vec2[i] = robots[i];
     }
-    for (; i < n; i++)
+    i = robotIndex;
+    for (; i < vec2.size(); i++)
     {
         vec2[i] = robots[i + 1];
     }
     
     return vec2;
+}
+
+bool check_input(int n, int buyers)
+{
+    if (buyers >= n)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
